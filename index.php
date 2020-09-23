bb29c1f54dac74d8965dea53b5f7ac2f076a10ef
Anthony O. Osawere <anthony@osawere.com>
2020-09-23 21:40:16 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require 'zern/bond/mobile.inc';
-require 'zern/libry/class/session.inc';
-require 'zern/libry/class/ip.inc';
+require 'zern/libry/class/instance/session.inc';
+require 'zern/libry/class/static/ssl.inc';
+require 'zern/libry/class/static/ip.inc';
 require 'debug.inc';
 ?>
