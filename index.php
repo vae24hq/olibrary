32d9b345948ed3cae82f8c5fd55cd469c8980daa
Anthony O. Osawere <anthony@osawere.com>
2020-09-24 06:57:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,11 @@ require 'libry/class/static/exit.inc';
 require 'libry/class/static/file.inc';
 require 'libry/class/static/redirect.inc';
 require 'libry/class/static/ssl.inc';
+require 'libry/class/static/string.inc';
 require 'libry/class/static/json.inc';
 require 'libry/class/static/random.inc';
 require 'libry/class/static/ip.inc';
+require 'libry/class/static/url.inc';
 require 'libry/class/instance/database.inc';
 
 require '_debug.inc';
