1aac07f08fec5566803eed4380d7c0d725d5582e
Anthony O. Osawere <anthony@osawere.com>
2020-09-24 05:54:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php
-require 'zern/bond/mobile.inc';
-
-require 'zern/libry/class/instance/session.inc';
-require 'zern/libry/class/static/print.inc';
-require 'zern/libry/class/static/exit.inc';
-require 'zern/libry/class/static/file.inc';
-require 'zern/libry/class/static/redirect.inc';
-require 'zern/libry/class/static/ssl.inc';
-require 'zern/libry/class/static/json.inc';
-
-require 'zern/libry/class/static/ip.inc';
+require 'libry/bond/mobile.inc';
+require 'libry/class/instance/session.inc';
+require 'libry/class/static/print.inc';
+require 'libry/class/static/exit.inc';
+require 'libry/class/static/file.inc';
+require 'libry/class/static/redirect.inc';
+require 'libry/class/static/ssl.inc';
+require 'libry/class/static/json.inc';
+require 'libry/class/static/random.inc';
+require 'libry/class/static/ip.inc';
 
 require '_debug.inc';
 ?>
