3e80e8e86d68955956a537a1168832799cee99f5
Anthony O. Osawere <anthony@osawere.com>
2020-09-24 11:22:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php
 require 'libry/bond/mobile.inc';
 require 'libry/class/instance/session.inc';
+require 'libry/class/static/string.inc';
 require 'libry/class/static/print.inc';
 require 'libry/class/static/exit.inc';
 require 'libry/class/static/file.inc';
 require 'libry/class/static/redirect.inc';
 require 'libry/class/static/ssl.inc';
-require 'libry/class/static/string.inc';
 require 'libry/class/static/json.inc';
 require 'libry/class/static/random.inc';
 require 'libry/class/static/ip.inc';
 require 'libry/class/static/url.inc';
+require 'libry/class/static/currency.inc';
+require 'libry/class/static/format.inc';
 require 'libry/class/instance/database.inc';
 
 require '_debug.inc';
