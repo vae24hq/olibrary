488f84ff7e23866a6c95ebcdf0eba21ff35cff10
Anthony O. Osawere <anthony@osawere.com>
2020-09-27 12:00:55 +0100

    SQL Snip

--- _o/dev9/box/snip/sql/ResetID.sql
+++ _o/dev9/box/snip/sql/ResetID.sql
@@ -1,6 +1,7 @@
-SET @newid=0;
-UPDATE `transfer` SET `id`=(@newid:=@newid+1) ORDER BY `id`;
+-- RESET & RENUMBER PRIMARY KEY
+-------------------------------
+SET @NewID = 0;
+UPDATE `TABLE_NAME` SET `ID_COLUMN`=(@NewID := @NewID +1) ORDER BY `ID_COLUMN`;
+SELECT MAX(`ID_COLUMN`) AS `IDMax` FROM `TABLE_NAME`;
+ALTER TABLE `TABLE_NAME` AUTO_INCREMENT = [IDMax + 1];
 
-SELECT MAX( `id` ) AS `IDMax` FROM `transfer`;
-
-ALTER TABLE `transfer` AUTO_INCREMENT = number;
