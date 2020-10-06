36f2026f5ca53e884b6707549df61888f6f11c2b
Anthony O. Osawere <anthony@osawere.com>
2020-10-06 18:54:48 +0100

    Update sql.php

--- tidy/review/sql.php
+++ tidy/review/sql.php
@@ -1,4 +1,27 @@
 <?php
+class oSQL2 {
+	public static function escape($input){
+		global $oDatabase;
+		return $oDatabase->real_escape_string($input);
+	}
+
+	public static function run($sql){
+		global $oDatabase;
+		$rez['result'] = $oDatabase->query($sql);
+		if(!empty($oDatabase->error)){
+			$rez['errMsg'] = $oDatabase->error;
+			$rez['errNo'] = $oDatabase->errno;
+		}
+		else {
+			$rez['affectedRows'] = $oDatabase->affected_rows;
+			if(is_object($rez['result'])){
+				$rez['numRows'] = $rez['result']->num_rows;
+			}
+		}
+		return $rez;
+	}
+}
+
 class oSQL {
 	public static function insert($table, $dataset, $option='oPREPARED'){
 		$query = '';
