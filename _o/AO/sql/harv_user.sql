27a18c55f0e58b66a9faae835aa2a947ad9c7f7b
Anthony O. Osawere <anthony@osawere.com>
2020-10-15 13:54:59 +0100

    Update harv_user.sql

--- _o/AO/sql/harv_user.sql
+++ _o/AO/sql/harv_user.sql
@@ -26,16 +26,16 @@ CREATE TABLE IF NOT EXISTS `harv_user` (
   PRIMARY KEY (`auid`),
   UNIQUE KEY `euid` (`euid`),
   UNIQUE KEY `suid` (`suid`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `phone` (`phone`),
+  UNIQUE KEY `username` (`username`),
   KEY `puid` (`puid`),
   KEY `stamp` (`stamp`),
   KEY `author` (`author`),
   KEY `status` (`status`),
   KEY `logid` (`logid`),
   KEY `stage` (`stage`),
-
-    KEY `email` (`email`),
-    KEY `phone` (`phone`),
-  KEY `username` (`username`),
+  KEY `password` (`password`),
   KEY `pin` (`pin`),
   KEY `type` (`type`),
   KEY `role` (`role`),
@@ -46,5 +46,11 @@ CREATE TABLE IF NOT EXISTS `harv_user` (
   KEY `birthdate` (`birthdate`),
   KEY `FK_UserParish` (`parisho`),
   CONSTRAINT `FK_UserParish` FOREIGN KEY (`parisho`) REFERENCES `harv_parish` (`suid`)
-  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC;
+SET FOREIGN_KEY_CHECKS = 1;
+
+
+SET FOREIGN_KEY_CHECKS = 0;
+REPLACE INTO `harv_user` (`auid`, `euid`, `suid`, `puid`, `stamp`, `author`, `status`, `logid`, `stage`, `parisho`, `email`, `phone`, `username`, `password`, `pin`, `type`, `role`, `surname`, `firstname`, `othername`, `gender`, `birthdate`) VALUES
+	(1, 'I0eQy5qi3TW3zn64jKR6N62agxVAhrm6ptOdPXc3', 'F6gGJ2H563ehDa1b50LS1cPNOdxj4A23980415671602766353xRso9563OeYpk5i3WZTh', 'mxWA3uwH7o26OpTDa4B3', '2020-10-15 13:52:33', 'oHarvest', NULL, NULL, 'unverified', 'VwQBYjAH92600gRSkaZL7OU7N5G4lC75301892461602702408BPg0XKNc2Sa3OMY9bLiG', 'ao@vae24.co', '09026636728', 'ao', '$2y$10$LnEmjsdWa0WV0yT52NSV5.q4Pt86.VZGS/0AWd./bNejZ6HE5raT6', '2240', 'standard', 'basic', 'Osawere', 'Anthony', 'O', 'male', '1987-10-31 13:53:24');
 SET FOREIGN_KEY_CHECKS = 1;
