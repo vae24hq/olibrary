5a1b6a111438e3950a90d66205952f3e88b71ed3
Anthony O. Osawere <anthony@osawere.com>
2020-10-20 01:33:18 +0100

    Update pdo.php

--- _o/AO/snippet/pdo.php
+++ _o/AO/snippet/pdo.php
@@ -8,3 +8,11 @@ $connect = new PDO(
 		PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
 	)
 );
+
+
+
+
+$dsn = 'mysql:dbname='.$data["database"].';host='.$data["host"].';charset=utf8'; #Data Source Name
+try {$connection = new PDO($dsn, $data["user"], $data["password"], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));}
+catch (PDOException $e){die('Connection failed: ' . $e->getMessage());}
+return $connection;
