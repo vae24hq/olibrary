4f92f204b400e9475d057c79401fa256b023b975
Anthony O. Osawere <anthony@osawere.com>
2020-11-05 01:24:13 +0100

    Delete sql.sql

--- tidy/active/sql.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `firm`
-CHANGE COLUMN `sendmail` `sendmail` CHAR(50) NULL DEFAULT 'no' AFTER `lang`;
-
-
-ALTER TABLE `firm`
-ADD COLUMN `sendmail` CHAR(50) NULL DEFAULT 'no' AFTER `lang`;
