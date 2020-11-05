9ca3dc4bd2cc5355a43138efff7452f0d89a21bf
Anthony O. Osawere <anthony@osawere.com>
2020-11-05 06:29:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
 require 'ignite.inc';
 
 #INITIALIZE PROJECT (based on a specific project)
-$initpath = oRoute::Path('init', true); #@NOTE ~ if TRUE - omain is used when source directory is not found
-oFile::Inc($$initpath, 'iOptional'); #@TODO ~ do full testing ON route's methods using this option
+$initpath = oRoute::Path('init', true); #@NOTE ~ when set to TRUE, omain is used if source directory is not found
+oFile::Inc($initpath, 'iRequired'); #@TODO ~ do full testing ON route's methods using sub domain
 
 
 #DEVBOX FILE - for development, demo & testing
-oFile::Inc(oROOT.'_o'.DS.'ignor'.DS.'_debug.inc', 'isOptional');
+oFile::Inc(oROOT.'_o'.DS.'ignor'.DS.'_debug.inc', 'iOptional');
 ?>
