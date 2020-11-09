0ee33abefcee19ef15e7b68e9bf9ee30dbc1e72e
Anthony O. Osawere <anthony@osawere.com>
2020-11-09 01:15:34 +0100

    oci

---
+++ core/oci/indev/omain/_old/index.php
@@ -0,0 +1,85 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php include 'inc/bit/head.inc';?>
+</head>
+
+<body data-spy="scroll" data-offset="110">
+
+	<?php #include 'inc/slice/color.inc';?>
+
+	<!-- LOADER -->
+	<?php include 'inc/slice/loader.inc';?>
+	<!-- END LOADER -->
+
+	<!-- START HEADER -->
+	<?php include 'inc/slice/header.inc';?>
+	<!-- END HEADER -->
+
+	<!-- START SECTION BANNER -->
+	<?php include 'inc/slice/banner.inc';?>
+
+	<!-- END SECTION BANNER -->
+
+	<!-- START PRICE LIST SECTION -->
+	<?php include 'inc/page/rate.inc';?>
+	<!-- END PRICE LIST SECTION -->
+
+	<!-- START ABOUT SECTION -->
+	<?php include 'inc/page/about.inc';?>
+	<!-- END ABOUT SECTION -->
+
+	<!-- START WHY CHOOSE SECTION -->
+	<?php include 'inc/page/why.inc';?>
+	<!-- END WHY CHOOSE SECTION -->
+
+	<!-- START LIVE PRICE CHART SECTION -->
+	<?php include 'inc/slice/live.inc';?>
+
+	<!-- END LIVE PRICE CHART SECTION -->
+
+	<!-- START COUNTER SECTION -->
+	<?php include 'inc/slice/counter.inc';?>
+	<!-- END COUNTER SECTION -->
+
+	<!-- START GET STARTED SECTION -->
+	<?php include 'inc/slice/steps.inc';?>
+	<!-- END GET STARTED SECTION -->
+
+	<!-- SRTAT VIDEO SECTION -->
+	<?php #include 'inc/slice/video.inc';?>
+	<!-- END VIDEO SECTION -->
+
+	<!-- START TEAM SECTION -->
+	<?php include 'inc/page/team.inc';?>
+	<!-- END TEAM SECTION -->
+
+	<!-- START BLOG SECTION -->
+
+	<?php #include 'inc/page/blog.inc';?>
+
+	<!-- END BLOG SECTION -->
+
+	<!-- START PARTNER SECTION -->
+
+	<?php include 'inc/slice/partner.inc';?>
+	<!-- END PARTNER SECTION -->
+
+	<!-- START CONTACT FORM SECTION -->
+	<?php include 'inc/page/contact.inc';?>
+	<!-- END CONTACT FORM SECTION -->
+
+	<!-- START FOOTER SECTION -->
+	<footer>
+		<div class="top_footer">
+			<?php include 'inc/slice/footer.inc';?>
+			<?php include 'inc/slice/copyright.inc';?>
+		</footer>
+		<!-- END FOOTER SECTION -->
+
+		<!-- <a href="#" class="scrollup" style="display: none;"><i class="fa fa-angle-up"></i></a> -->
+		<?php include 'inc/bit/js.inc';?>
+	</body>
+	</html>
