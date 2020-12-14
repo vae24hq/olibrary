20d546b79be14061c8ce3cd240288bc747a38c3b
Anthony O. Osawere <anthony@osawere.com>
2020-12-14 22:40:04 +0100

    Create demo.php

---
+++ _o/dev9/framework/dotphp/PHPMaster/_source/xend/demo/demo.php
@@ -0,0 +1,13 @@
+<?php
+/* CREATE USERS
+echo '<br>username: '.Utility::username('record');
+echo '<br>password: '.Utility::password('record123');
+echo '<br>type: '.Utility::username('record');
+echo '<br>puid: '.Utility::randomiz('puid');
+echo '<br>ruid: '.Utility::randomiz('ruid');*/
+
+// include oMODEL.'record.php';
+// $demo = record::isHospitalNo('12345');
+// utility::dbog($demo);
+// die();
+?>
